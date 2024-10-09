da5c98e8f119d8af3703a4ac40046ef05420e79a
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-10-09 11:09:13 -0700

    10 minute timestamp

--- Lab21.cpp
+++ Lab21.cpp
@@ -1,4 +1,23 @@
+/*  Lab 21: Modifying dll demo to create a goat class
+    Objectives: Amend the code as follows:
+        1. The Goat class will have two constructors, as expected:
+            + The default constructor will randomly assign:
+                - an age, 1-20
+                - a name, randomly selected from the 15-element names[] array
+                - a color, randomly selected from the 15-element colors[] array
+        2. The parameter constructor will be a typical 3-element parameter setup.
+        3. Modify the DoublyLinkedList class's push_front() and push_back() functions such that it has a Goat object as a parameter, rather than an int. 
+        4. In main(), create a DoublyLinkedList object. Append to it a random number of Goat objects, range 5-20 or so.
+        5. In main(), call your methods to print both forward and backward to show the proper traversals.
+        6. Use srand(time(0)); as one of your first lines in main() to randomize your random numbers.
+        7. Update both of the print() methods that are in the class. Both should display the text "List is empty" if the list is empty; otherwise, output the list as shown in the sample output below.
+
+    Design decisions:
+        
+*/
+
 #include <iostream>
+#include <ctime> // For time needed by srand
 using namespace std;
 
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
@@ -131,6 +150,7 @@ public:
 
 // Driver program
 int main() {
+
     DoublyLinkedList list;
     int size = rand() % (MAX_LS-MIN_LS+1) + MIN_LS;
 
